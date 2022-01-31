f9586cbff40b358531216a30f9d0b1e8cc19392b
Martha Richardson <mgsrichard@yahoo.com>
2022-01-31 12:28:03 -0500

    deliverable 2 done, 3 started

--- scraping.py
+++ scraping.py
@@ -20,6 +20,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": mars_hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -107,6 +108,63 @@ def mars_facts():
 
     #Convert table back to html, add bootstrap
     return df.to_html()
+
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+
+
+    #parse main page 
+    html = browser.html
+    mainpg_soup = soup(html,'html.parser')
+    #print(mainpg_soup.prettify())
+
+
+
+    #find tags for links to four sub pages with images
+    div_items = mainpg_soup.find_all('div',class_='item')
+    len(div_items)
+
+    #for loop to go through all four images
+    #loop through all 4 sub pages, capturing image links and titles
+    for item in div_items:
+        
+        #get the relative url of the sub page with the hemisphere image
+        link = item.find('a')['href']
+        
+        #construct the whole/absolute url
+        absolute_url = url+link
+        
+        #grab the title of the link
+        title = item.find('h3').text
+        
+        #go visit the sub page
+        browser.visit(absolute_url)
+        
+        #parse sub page
+        html_sub=browser.html
+        
+        #convert sub page to a beautiful soup
+        sub_soup=soup(html_sub,'html.parser')
+        
+        #find and capture the url for the jpg image
+        hemi_img = sub_soup.find('li').find('a')['href']
+        
+        #convert image relative url to absolute url
+        hemi_img_abs_url = url+hemi_img
+        
+        #append to list of dictionaries of hemisphere image urls and titles
+        hemisphere_image_urls.append({"img_url":hemi_img_abs_url,"title":title})
+
+    return hemisphere_image_urls
+
 
 
 if __name__ == "__main__":
